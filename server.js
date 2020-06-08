3aa1b4ddf0ac3673f169c4c289600215ae35c4a0
unknown <jdineshit@gmail.com>
2020-06-08 13:58:00 +0530

    Cloud deployment changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist/mars-rover'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/mars-rover/'}
+  );
+  });
+
+app.listen(process.env.PORT || 8080, () => console.log(`Application is listening at port: ${port}`));
